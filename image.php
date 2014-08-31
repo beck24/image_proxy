01b55836b63ce0836954a6b59bf0ef23f37a79b6
Matt Beckett <beck24@gmail.com>
2014-08-31 02:48:54 -0700

    initial commit

---
+++ image.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * image proxy
+ * 
+ * 
+ */
+
+namespace image_proxy;
+
+// Get DB settings
+require_once(dirname(dirname(dirname(__FILE__))) . '/engine/settings.php');
+
+global $CONFIG;
+
+$url = urldecode($_GET['url']);
+$token = $_GET['token'];
+
+if (!$CONFIG->image_proxy_secret) {
+	$mysql_dblink = @mysql_connect($CONFIG->dbhost, $CONFIG->dbuser, $CONFIG->dbpass, true);
+	if ($mysql_dblink) {
+		if (@mysql_select_db($CONFIG->dbname, $mysql_dblink)) {
+			$result = mysql_query("select name, value from {$CONFIG->dbprefix}datalists where name = '__site_secret__'", $mysql_dblink);
+			if ($result) {
+				$row = mysql_fetch_object($result);
+				while ($row) {
+					if ($row->name == '__site_secret__') {
+						$site_secret = $row->value;
+					}
+					$row = mysql_fetch_object($result);
+				}
+			}
+
+			@mysql_close($mysql_dblink);
+		}
+	}
+}
+else {
+	$site_secret = $CONFIG->image_proxy_secret;
+}
+
+if (!$site_secret) {
+	error_log('Cannot find site secret');
+	header('HTTP/1.0 404 Not Found');
+	exit;
+}
+
+if ($token !== md5($site_secret . $url)) {
+	error_log('Invalid proxy token');
+	header('HTTP/1.0 404 Not Found');
+	exit;
+}
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5); // in seconds
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_NOBODY, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$headers = curl_exec($ch);
+
+if ($headers === false) {
+	// we couldn't get the headers from the remote url
+	error_log('Could not retrieve remote headers');
+	header('HTTP/1.0 404 Not Found');
+	exit;
+}
+
+foreach (explode("\r\n", $headers) as $hdr) {
+	header($hdr);
+}
+
+readfile($url);
+exit;
---
+++ start.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace image_proxy;
+
+define('PLUGIN_ID', 'image_proxy');
+
+elgg_register_event_handler('init', 'system', __NAMESPACE__ . '\\init');
+
+function init() {
+	elgg_register_plugin_hook_handler('view', 'all', __NAMESPACE__ . '\\view_hook');
+}
+
+/**
+ * replace any http images with https urls
+ * 
+ * @param type $h
+ * @param type $t
+ * @param type $r
+ * @param type $p
+ * @return type
+ */
+function view_hook($h, $t, $r, $p) {
+
+	if (preg_match_all( '/<img[^>]+src\s*=\s*["\']?([^"\' ]+)[^>]*>/', $r, $extracted_image)) {
+		foreach ($extracted_image[0] as $key => $i) {
+			
+			if (strpos($extracted_image[1][$key], elgg_get_site_url()) !== false) {
+				continue; // already one of our links
+			}
+			
+			if (!is_https($extracted_image[1][$key])) {
+				// replace this url
+				$url = urlencode($extracted_image[1][$key]);
+				$token = get_token($extracted_image[1][$key]);
+				$new_url = elgg_normalize_url('mod/' . PLUGIN_ID . '/image.php?url=' . $url . '&token=' . $token);
+				
+				$replacement_image = str_replace($extracted_image[1][$key], $new_url, $i);
+				$r = str_replace($i, $replacement_image, $r);
+			}
+		}
+	}
+	
+	return $r;
+}
+
+/**
+ * detect if the current page is using https
+ * 
+ * @staticvar type $is_https
+ * @return type
+ */
+function is_https($url) {
+	static $is_https;
+	
+	if (!is_array($is_https)) {
+		$is_https = array();
+	}
+	
+	if (isset($is_https[$url])) {
+		return $is_https[$url];
+	}
+	
+	$parts = parse_url($url);
+	
+	$is_https[$url] = strtolower($parts['scheme']) === 'https';
+	
+	return $is_https[$url];
+}
+
+
+function get_token($url) {
+	global $CONFIG;
+	
+	if ($CONFIG->image_proxy_secret) {
+		$site_secret = $CONFIG->image_proxy_secret;
+	}
+	else {
+		$site_secret = get_site_secret();
+	}
+	return md5($site_secret . $url);
+}
