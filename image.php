d3794996dce094fcbd7a1290c318027ca78e7eb0
Matt Beckett <beck24@gmail.com>
2018-04-18 02:44:30 -0700

    upgrade for Elgg 3, apply mrclay fixes

--- image.php
+++ image.php
@@ -4,58 +4,38 @@ namespace image_proxy;
 
 error_reporting(0); // we don't want notices etc to break the image data passthrough
 
-// Get DB settings pre 2.0
-$settings = dirname(dirname(dirname(__FILE__))) . '/engine/settings.php';
-if (!file_exists($settings)) {
-	// try from root location
-	$settings = dirname(dirname(dirname(__FILE__))) . '/settings.php';
-}
+$settings = dirname(dirname(dirname(__FILE__))) . '/elgg-config/settings.php';
 
-if (!file_exists($settings)) {
+$fail = function() {
 	header('Content-Type: image/png');
 	readfile('graphics/proxyfail.png');
 	exit;
+};
+
+if (!file_exists($settings)) {
+	$fail();
 }
 
 require_once($settings);
 
 global $CONFIG;
 
-$url = urldecode($_GET['url']);
+$url = $_GET['url'];
 $token = $_GET['token'];
+$secret = '';
 
 if ($CONFIG->image_proxy_secret) {
-	$site_secret = $CONFIG->image_proxy_secret;
+	$secret = $CONFIG->image_proxy_secret;
 } else {
-	$mysql_dblink = @mysql_connect($CONFIG->dbhost, $CONFIG->dbuser, $CONFIG->dbpass, true);
-	if ($mysql_dblink) {
-		if (@mysql_select_db($CONFIG->dbname, $mysql_dblink)) {
-			$result = mysql_query("select name, value from {$CONFIG->dbprefix}datalists where name = '__site_secret__'", $mysql_dblink);
-			if ($result) {
-				$row = mysql_fetch_object($result);
-				while ($row) {
-					if ($row->name == '__site_secret__') {
-						$site_secret = $row->value;
-					}
-					$row = mysql_fetch_object($result);
-				}
-			}
-
-			@mysql_close($mysql_dblink);
-		}
-	}
+	$secret = file_get_contents($CONFIG->dataroot . 'image_proxy_secret.txt');
 }
 
-if (!$site_secret) {
-	header('Content-Type: image/png');
-	readfile('graphics/proxyfail.png');
-	exit;
+if (!$secret) {
+	$fail();
 }
 
-if ($token !== sha1($site_secret . $url)) {
-	header('Content-Type: image/png');
-	readfile('graphics/proxyfail.png');
-	exit;
+if ($token !== sha1($secret . $url)) {
+	$fail();
 }
 
 $ch = curl_init();
@@ -69,14 +49,50 @@ $headers = curl_exec($ch);
 
 if ($headers === false) {
 	// we couldn't get the headers from the remote url
-	header('Content-Type: image/png');
-	readfile('graphics/proxyfail.png');
-	exit;
+	$fail();
 }
 
+// we must find a content type of image
+$found_img = false;
+$forwarded_headers = [];
+
 foreach (explode("\r\n", $headers) as $header) {
+	if (strpos($header, 'HTTP/') === 0) {
+		// In case of redirects, we only consider headers after the last response line,
+		// so we reset state here.
+		$found_img = false;
+		$forwarded_headers = [];
+		continue;
+	}
+
+	if (false === strpos($header, ':')) {
+		continue;
+	}
+
+	list ($name, $value) = explode(':', $header, 2);
+	$lower_name = strtolower($name);
+	$value = trim($value);
+
+	if ($lower_name === 'content-type' && strpos($value, 'image/') === 0) {
+		$found_img = true;
+		$forwarded_headers[] = $header;
+		continue;
+	}
+
+	if (in_array($lower_name, ['age', 'cache-control', 'date', 'expires', 'last-modified'])) {
+		$forwarded_headers[] = $header;
+	}
+}
+
+if (!$found_img) {
+	$fail();
+}
+
+foreach ($forwarded_headers as $header) {
 	header($header);
 }
 
+// TODO: eliminate need for PHP to make 2nd request here. Probably would want to use
+// streams: fopen(), stream_get_meta_data()['wrapper_data'], and fpassthru().
 readfile($url);
 exit;
--- start.php
+++ start.php
@@ -2,56 +2,59 @@
 
 namespace image_proxy;
 
-elgg_register_event_handler('init', 'system', __NAMESPACE__ . '\\init');
-
-function init() {
-	//@TODO - can we do this without parsing all views?
+elgg_register_event_handler('init', 'system', function() {
 	elgg_register_plugin_hook_handler('view', 'all', __NAMESPACE__ . '\\view_hook');
-}
+});
 
 /**
  * replace any http images with https urls
  * 
- * @param type $h
- * @param type $t
- * @param type $r
- * @param type $p
- * @return type
+ * @param string $h "view"
+ * @param string $t view name
+ * @param string $html view output
+ * @param array $p hook params
+ * @return string
  */
-function view_hook($h, $t, $r, $p) {
+function view_hook($h, $t, $html, $p) {
+	if (false === strpos($html, '<img')) {
+		// exit fast for most views
+		return $html;
+	}
 	
 	$http_url = str_replace('https://', 'http://', elgg_get_site_url());
 
-	if (preg_match_all( '/<img[^>]+src\s*=\s*["\']?([^"\' ]+)[^>]*>/', $r, $extracted_image)) {
-		foreach ($extracted_image[0] as $key => $i) {
+	if (preg_match_all( '/<img[^>]+src\s*=\s*["\']?([^"\' ]+)[^>]*>/', $html, $extracted_image)) {
+		foreach ($extracted_image[0] as $key => $img) {
+			$url = $extracted_image[1][$key];
 			
-			if (strpos($extracted_image[1][$key], elgg_get_site_url()) !== false) {
+			if (strpos($url, elgg_get_site_url()) !== false) {
 				continue; // already one of our links
 			}
 
 			// check if this is our url being requested over http, and rewrite to https
-			if (strpos($extracted_image[1][$key], $http_url) === 0) {
-				$https_image = str_replace('http://', 'https://', $extracted_image[1][$key]);
-				$replacement_image = str_replace($extracted_image[1][$key], $https_image, $i);
-				$r = str_replace($i, $replacement_image, $r);
+			if (strpos($url, $http_url) === 0) {
+				$new_url = str_replace('http://', 'https://', $url);
+				$replacement_image = str_replace($url, $new_url, $img);
+				$html = str_replace($img, $replacement_image, $html);
 				continue;
 			}
-			
-			if (!is_https($extracted_image[1][$key])) {
-				// replace this url
-				$url = urlencode($extracted_image[1][$key]);
-				if (strpos($url, 'http') === 0) {
-					$token = get_token($extracted_image[1][$key]);
-					$new_url = elgg_normalize_url('mod/image_proxy/image.php?url=' . $url . '&token=' . $token);
-					
-					$replacement_image = str_replace($extracted_image[1][$key], $new_url, $i);
-					$r = str_replace($i, $replacement_image, $r);
-				}
+
+			if (strpos($url, 'http:') === 0) {
+				// replace with proxy URL
+				// URL may contain "&amp;" or some other HTML encoded character
+				$real_url = html_entity_decode($url, ENT_HTML5, 'UTF-8');
+				$new_url = elgg_normalize_url('mod/image_proxy/image.php?') . http_build_query([
+					'url' => $real_url,
+					'token' => get_token($real_url),
+				]);
+				$new_url = htmlspecialchars($new_url, ENT_QUOTES, 'UTF-8');
+				$replacement_img = str_replace($url, $new_url, $img);
+				$html = str_replace($img, $replacement_img, $html);
 			}
 		}
 	}
 	
-	return $r;
+	return $html;
 }
 
 /**
@@ -80,12 +83,32 @@ function is_https($url) {
 
 
 function get_token($url) {
+	$secret = get_secret();
 	
+	return sha1($secret . $url);
+}
+
+
+function get_secret() {
+	static $secret;
+	if ($secret) {
+		return $secret;
+	}
+
 	if (elgg_get_config('image_proxy_secret')) {
-		$site_secret = elgg_get_config('image_proxy_secret');
+		$secret = elgg_get_config('image_proxy_secret');
 	}
 	else {
-		$site_secret = get_site_secret();
+		// generate our own
+		$dataroot = elgg_get_config('dataroot');
+		if (file_exists($dataroot . 'image_proxy_secret.txt')) {
+			$secret = file_get_contents($dataroot . 'image_proxy_secret.txt');
+		}
+		else {
+			$secret = _elgg_services()->crypto->getRandomString(128, \ElggCrypto::CHARS_PASSWORD);
+			file_put_contents($dataroot . 'image_proxy_secret.txt', $secret);
+		}
 	}
-	return sha1($site_secret . $url);
+
+	return $secret;
 }
